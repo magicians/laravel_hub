56e525e4374330384fabf8be691e770029df8884
Magician <magicians.alex@gmail.com>
2016-04-26 11:50:31 +0800

    no message
    Signed-off-by: Magician <magicians.alex@gmail.com>

--- config/app.php
+++ config/app.php
@@ -40,7 +40,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://laravel.app'),
+    'url' => env('APP_URL', 'http://fanshub.app'),
 
     /*
     |--------------------------------------------------------------------------
